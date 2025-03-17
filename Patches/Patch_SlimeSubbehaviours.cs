fd068ce915cc2daa359c63d2dfc00f3f10ee53a3
Lionmeow <49107380+Lionmeow@users.noreply.github.com>
2025-03-16 22:56:37 -0700

    improve subbehaviour patch
    + Made patch target all methods within slime behaviours, not just Action
    + Limited transpiler to only affect methods that add force to the slime

--- Patches/Patch_SlimeSubbehaviours.cs
+++ Patches/Patch_SlimeSubbehaviours.cs
@@ -12,10 +12,11 @@ namespace SRVR.Patches
         public static float GetReplacedFixedDeltaTime() => 0.025f;
 
         public static IEnumerable<MethodBase> TargetMethods() =>
-            AccessTools.AllTypes().Where(x => !x.IsAbstract && x.IsSubclassOf(typeof(SlimeSubbehaviour))).Select(x => AccessTools.Method(x, "Action"));
+            AccessTools.AllTypes().Where(x => !x.IsAbstract && x.IsSubclassOf(typeof(SlimeSubbehaviour)))
+            .Select(x => AccessTools.GetDeclaredMethods(x).Where(y => !y.Name.Contains("Update"))).SelectMany(x => x);
 
         public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions) =>
-            Transpilers.MethodReplacer(instructions, AccessTools.PropertyGetter(typeof(Time), "fixedDeltaTime"), 
-                AccessTools.Method(typeof(Patch_SlimeSubbehaviours), "GetReplacedFixedDeltaTime"));
+            instructions.Any(x => x.operand is MethodBase mb && mb.Name.Contains("AddForce")) ? Transpilers.MethodReplacer(instructions, AccessTools.PropertyGetter(typeof(Time), "fixedDeltaTime"), 
+                AccessTools.Method(typeof(Patch_SlimeSubbehaviours), "GetReplacedFixedDeltaTime")) : instructions;
     }
 }
