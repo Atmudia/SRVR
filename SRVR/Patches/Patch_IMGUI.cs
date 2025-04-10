bcfb49fc114ee4e15d9c3234738c52f9452fc71e
Lionmeow <49107380+Lionmeow@users.noreply.github.com>
2025-04-10 14:13:57 -0700

    small fixes
    + Implement IMGUI patch to hopefully fix lagspikes without fully removing OnGUI
    + Fixed UIDetector raycast being blocked by vacpack

---
+++ SRVR/Patches/Patch_IMGUI.cs
@@ -0,0 +1,35 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace SRVR.Patches
+{
+    [HarmonyPatch(typeof(GUILayoutUtility), "Begin")]
+    internal static class Patch_IMGUI
+    {
+        public static bool Prefix(GUILayoutUtility __instance, int instanceID)
+        {
+            GUILayoutUtility.LayoutCache layoutCache = GUILayoutUtility.SelectIDList(instanceID, false);
+            if (Event.current.type == EventType.Layout)
+            {
+                if (layoutCache.topLevel == null)
+                    layoutCache.topLevel = new GUILayoutGroup();
+                if (layoutCache.windows == null)
+                    layoutCache.windows = new GUILayoutGroup();
+
+                layoutCache.topLevel.entries.Clear();
+                layoutCache.windows.entries.Clear();
+
+                GUILayoutUtility.current.topLevel = layoutCache.topLevel;
+                GUILayoutUtility.current.layoutGroups.Clear();
+                GUILayoutUtility.current.layoutGroups.Push(GUILayoutUtility.current.topLevel);
+
+                return false;
+            }
+            GUILayoutUtility.current.topLevel = layoutCache.topLevel;
+            GUILayoutUtility.current.layoutGroups = layoutCache.layoutGroups;
+            GUILayoutUtility.current.windows = layoutCache.windows;
+
+            return false;
+        }
+    }
+}
